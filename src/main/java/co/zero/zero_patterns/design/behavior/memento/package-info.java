b5712ca5894634d8a077f395ed058c56fca1705b
Hernan <zerovirus23@gmail.com>
2016-01-22 09:27:05 -0500

    2016-01-22: Create all packages where pattern implementations going to be defined

---
+++ src/main/java/co/zero/zero_patterns/design/behavior/memento/package-info.java
@@ -0,0 +1,14 @@
+//=======================================================================
+// ARCHIVO package-info.java
+// FECHA CREACIÓN: Jan 22, 2016
+// AUTOR: Hernán Tenjo
+//=======================================================================
+
+/**
+ * This package contains an implementation of Memento pattern
+ * 
+ * <p>#{pattern_definition}</p>
+ * 
+ * Definition taken from #{pattern_definition_url}
+ */
+package co.zero.zero_patterns.design.behavior.memento;
---
+++ src/main/java/co/zero/zero_patterns/design/creation/abstract_factory/package-info.java
@@ -0,0 +1,13 @@
+//=======================================================================
+// ARCHIVO package-info.java
+// FECHA CREACIÓN: Jan 22, 2016
+// AUTOR: Hernán Tenjo
+//=======================================================================
+/**
+ * This package contains samples of how to implement behavioral patterns
+ * 
+ * <p>#{pattern meaning}</p>
+ * 
+ * Definition taken from http://www.javacodegeeks.com/2015/09/introduction-to-design-patterns.html
+ */
+package co.zero.zero_patterns.design.creation.abstract_factory;
