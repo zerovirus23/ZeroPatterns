c845def9f600bb6d1ac025cc04f08404e05870fd
Hernan <zerovirus23@gmail.com>
2016-01-26 15:22:20 -0500

    2016-01-26: Implementation of facade pattern

---
+++ src/main/java/co/zero/zero_patterns/design/structure/facade/ScheduleComplexClient.java
@@ -0,0 +1,47 @@
+//=======================================================================
+// FILE ScheduleComplexClient.java
+// CREATION DATE: Jan 26, 2016
+// AUTHOR: Hernán Tenjo
+//=======================================================================
+package co.zero.zero_patterns.design.structure.facade;
+
+
+/**
+ * This class represent the client that uses the low level server services to 
+ * start and stop the remote system
+ * @author Hernán Tenjo
+ * @version 1.0
+*/
+public class ScheduleComplexClient {
+	private ScheduleServer server;
+	
+	public ScheduleComplexClient(ScheduleServer server) {
+		this.server = server;
+	}
+	
+	/**
+	 * Process required by the client to init the system provider
+	 */
+	public void startSystem(){
+		server.createSystemObjects();
+		server.readSystemConfigFiles();
+		server.initVirtualMachine();
+		server.initializeContext();
+		server.initializeListeners();
+		server.initQueues();
+		server.createSystemObjects();
+		System.out.println("Complex Client --> Remote system started");
+	}
+	
+	/**
+	 * Process required by the client to stop the stop provider
+	 */
+	public void stopSystem(){
+		server.stopServices();
+		server.stopVirtualMachine();
+		server.stopSystems();
+		server.shutdown();
+		System.out.println("Complex Client --> Remote system stoped");
+	}
+}
+
--- src/main/java/co/zero/zero_patterns/design/structure/facade/package-info.java
+++ src/main/java/co/zero/zero_patterns/design/structure/facade/package-info.java
@@ -7,8 +7,16 @@
 /**
  * This package contains an implementation of Facade pattern
  * 
- * <p>#{pattern_definition}</p>
+ * <p>The Facade Pattern makes a complex interface easier to use, using a Facade class. 
+ * The Facade Pattern provides a unified interface to a set of interface in a subsystem. 
+ * Facade defines a higher-level interface that makes the subsystem easier to use.
+ * The Facade unifies the complex low-level interfaces of a subsystem in-order to provide 
+ * a simple way to access that interface. It just provides a layer to the complex interfaces 
+ * of the sub-system which makes it easier to use.The Facade do not encapsulate the subsystem 
+ * classes or interfaces; it just provides a simplified interface to their functionality. 
+ * A client can access these classes directly. It still exposes the full functionality of 
+ * the system for the clients who may need it.</p>
  * 
- * Definition taken from #{pattern_definition_url}
+ * Definition taken from <a href="http://www.javacodegeeks.com/2015/09/facade-design-pattern.html">URL Pattern</a>
  */
 package co.zero.zero_patterns.design.structure.facade;
